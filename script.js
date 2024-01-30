1a55ffcd10962a8ff44355a8e643580320031545
philipbenn <phil658c@stud.kea.dk>
2024-01-30 10:28:30 +0100

    final commit

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 var min = 1;
 var max = 100;
 var computerGuess;
+var guessList = document.getElementById('guess-list');
 
 function getRandomNumber(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
@@ -9,6 +10,15 @@ function getRandomNumber(min, max) {
 function makeGuess() {
     computerGuess = getRandomNumber(min, max);
     document.getElementById('computer-guess').innerText = 'Computer is guessing: ' + computerGuess;
+
+    var listItem = document.createElement('li');
+    listItem.textContent = 'Computer guessed ' + computerGuess;
+
+    guessList.appendChild(listItem);
+}
+
+function clearGuessList() {
+    guessList.innerHTML = '';
 }
 
 function checkGuess(result) {
@@ -17,7 +27,13 @@ function checkGuess(result) {
     } else if (result === 'lower') {
         max = computerGuess - 1;
     } else if (result === 'correct') {
+        var listItem = document.createElement('li');
+        listItem.textContent = 'Computer guessed ' + computerGuess + ', which was correct.';
+        guessList.appendChild(listItem);
+        
         alert('Correct! The Computer guessed the number.');
+        clearGuessList();
+        makeGuess();
         return;
     }
 
